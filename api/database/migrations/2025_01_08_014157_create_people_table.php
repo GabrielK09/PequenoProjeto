99bcd5d7c3ccfb580fdd4b4ac494869646c1a2c0
TsukiKtk <87103106+TsukiKtk@users.noreply.github.com>
2025-01-07 23:01:14 -0300

    Initial commit

---
+++ api/database/migrations/2025_01_08_014157_create_people_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 200);
+            $table->boolean('type', 1);
+            $table->string('cpf', 11)->nullable();
+            $table->string('cnpj', 14)->nullable();
+            $table->string('cep', 8);
+            $table->string('address', 200);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('people');
+    }
+};
