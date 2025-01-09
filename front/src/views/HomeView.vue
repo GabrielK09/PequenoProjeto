5a6252f784a63e5fcc5a927efb7ea522057b457b
TsukiKtk <87103106+TsukiKtk@users.noreply.github.com>
2025-01-08 23:12:06 -0300

    Front nd bom

---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>
+    Digite seu CEP
+  </h1>
+
+  <input
+    type="text"
+    v-model="cep"
+  />
+
+</template>
+
+<script>
+/* eslint-disable */
+import { axios } from 'axios';
+
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      cep: null,
+    };
+  },
+  methods: {
+    async getData() {
+      const response = await axios.get(`viacep.com.br/ws/${this.cep}/json/`);
+      console.log(response)
+    },
+  },
+};
+</script>
