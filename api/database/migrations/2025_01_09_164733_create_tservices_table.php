16b8263509162e12f74dc99c8163d58984a291ff
Gabriel Kochem <gabikochem55@gmail.com>
2025-01-10 07:52:12 -0300

    A

--- api/database/migrations/2025_01_09_164733_create_tservices_table.php
+++ api/database/migrations/2025_01_09_164733_create_tservices_table.php
@@ -6,27 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('tservices', function (Blueprint $table) {
             $table->id();
-            $table->string('product', 100);
+            $table->string('service', 100);
             $table->string('description', 200);
-            $table->float('qtde', 15, 4);
             $table->float('sale_price', 10, 2);
-            $table->string('image', 100);
-            $table->string('cfop', 4);
-            $table->string('csosn', 3);
-            $table->string('ncm', 50);
-            $table->float('icms', 40);
-
+            $table->float('iss', 10, 2);
             $table->timestamps();
-
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('services');
     }
 };
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -3,28 +3,57 @@
     Digite seu CEP
   </h1>
 
-  <input
-    type="text"
-    v-model="cep"
-  />
+  <form @submit.prevent="getData()">
+    <input
+      type="text"
+      v-model="cep"
+      maxlength="8"
+    />
 
+    <br>
+    <button
+      type="submit"
+    >
+      Buscar
+    </button>
+
+  </form>
+
+  <div v-if="dataResponse != ''">
+    <h3>CEP: {{dataResponse.cep}}</h3>
+    <h3>Logradouro: {{dataResponse.logradouro}}</h3>
+    <h3>Bairro: {{dataResponse.bairro}}</h3>
+    <h3>Cidade: {{dataResponse.localidade}}</h3>
+    <h3>UF: {{dataResponse.uf}}</h3>
+    <h3>Estado: {{dataResponse.estado}}</h3>
+    <h3>Código IBGE: {{dataResponse.ibge}}</h3>
+  </div>
 </template>
 
 <script>
 /* eslint-disable */
-import { axios } from 'axios';
+import axios from 'axios';
 
 export default {
   name: 'HomeView',
   data() {
     return {
-      cep: null,
+      cep: '',
+      dataResponse: []
     };
   },
   methods: {
     async getData() {
-      const response = await axios.get(`viacep.com.br/ws/${this.cep}/json/`);
-      console.log(response)
+      try {
+        const response = await axios.get(`https://viacep.com.br/ws/${this.cep}/json`);
+        this.dataResponse = response.data
+        console.log(this.dataResponse)
+        
+      } catch (error) {
+        console.error(error)  
+
+      }
+      
     },
   },
 };
