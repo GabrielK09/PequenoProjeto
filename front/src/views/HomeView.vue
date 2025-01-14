11717164e9976bf7823ea293c8026228f2a9db99
Gabriel Kochem <gabikochem55@gmail.com>
2025-01-14 18:00:35 -0300

    A

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,24 +1,35 @@
 <template>  
-  <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-32 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+  <aside id="default-sidebar" class="fixed top-0 left-0 z-1 w-32 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
     <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
       <ul class="space-y-2 font-medium">
           <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">   
-              <span class="ms-3">Dashboard</span>
-              
-            </a>
+            <span class="flex p-1 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">              
+                <button 
+                  @click="report()"
+                  class="ml-4"
+                >Reportar</button>
+
+              </span>
+            
           </li>         
       </ul>
     </div>
   </aside>
 
   <div class="p-4 sm:ml-32">
-    <header>
+    <header class="flex bg-gray-800 p-4">
       <h1>Grupo Roxo</h1>
-    </header>
 
+    </header>
+  
+    <FormReport
+      v-if="this.show"
+      :show="this.show"
+
+    />
   </div>
 
+  
 
 <!--img
     src="https://s2-g1.glbimg.com/kpFpybZz9WaqZDJUkar4-6sTs6k=/0x0:1777x937/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2018/R/2/SSQNLtTMOVDwtVzCiqGQ/vingadores-guerra-infinita-thanos.jpg"
@@ -37,6 +48,7 @@
    
     data(){
       return {
+        show: true
 
       }
 
@@ -46,16 +58,10 @@
 
     },
     methods: {
-      async test()
-      {
-        try {
-          const response = await axios.get('https://viacep.com.br/ws/89711226/json/')        
-          console.log(response.data)  
+      report() {
+        
+        this.show = !this.show
 
-        } catch (error) {
-          console.error(error)
-
-        }
       }
     },
 
--- front/src/views/components/FormReport.vue
+++ front/src/views/components/FormReport.vue
@@ -1,33 +1,43 @@
 <template>
-    <h1>Aq</h1>
-    <div>
-        <form action="">
-            <label>Título do report: </label>
-            <input 
-                type="text" 
-                class="border-2 border-black rounded-md"
-                v-model="title"
-                
-            />
-            <br>
-
-            <label>Descrição: </label>
-            <input 
-                type="text" 
-                class="border-2 border-black rounded-md"
-                v-model="description"
-                
-            />
-            <br>
-
-            <select>
-                <option value="" disabled selected>Selecione o Grupo</option>
-            </select>
-            <br>
-        
-            <input type="file"
+    <div class="mt-5 w-full max-w-xs">
+        <form action="" class="bg-gray-600 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div class="mb-2">
+                <label class="block text-white text-sm font-bold mb-2">Título do card: </label>
+                <input 
+                    type="text" 
+                    class="shadow appearance-none border rounded w-full py-2 px-1 leading-tight focus:outline-none focus:shadow-outline"
+                    placeholder="..."
+                    v-model="title"
+                    
+                />
+            </div>
             
-            />
+            <div class="mb-2">
+                <label class="block text-white text-sm font-bold mb-2">Descrição: </label>
+                <input 
+                    type="text" 
+                    class="shadow appearance-none border rounded w-full py-2 px-1 leading-tight focus:outline-none focus:shadow-outline"
+                    v-model="description"
+                    
+                />
+            </div>
+            
+            <div class="mb-2">
+                <select class="shadow border rounded py-2 px-1">
+                    <option value="" disabled selected>Selecione o Grupo</option>
+                </select>
+            </div>
+
+            <div class="max-w-40">
+                <label for="fileUpload" class="block text-white text-xl py-2 font-bold hover:bg-gray-400 w-40">Enviar arquivo</label>
+                <input
+                    id="fileUpload"
+                    type="file"
+                    class="hidden"
+                
+                />
+                <p v-if="fileName"></p>
+            </div>
 
         </form>
     </div>
@@ -36,7 +46,19 @@
 <script>
     export default {
         name: "FormReport",
+        data(){
+            return {
+        
+            }
+        },
 
+        props: {
+            show: {
+                type: Boolean,
+                required: true
+
+            }
+        }
     }
 
 </script>
